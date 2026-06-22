e30f21579d6633a666f94cac17b8ae51c0fcbed3
zxred <germ87891@gmail.com>
2026-06-22 20:09:05 +0700

    Add files via upload

---
+++ connection_mgr.py
@@ -0,0 +1,373 @@
+# uncompyle6 version 3.9.3
+# Python bytecode version base 2.7 (62211)
+# Decompiled from: Python 3.12.7 (tags/v3.12.7:0b05ead, Oct  1 2024, 03:06:41) [MSC v.1941 64 bit (AMD64)]
+# Embedded file name: C:\Users\fsociety\Desktop\unpack\MY_scripts_unpacked\scripts - client\connection_mgr.py
+# Compiled at: 2026-06-21 15:10:10
+import hashlib, json, ResMgr, BigWorld, constants, pwd_token
+from Event import Event, EventManager
+from PlayerEvents import g_playerEvents
+from debug_utils import LOG_DEBUG, LOG_NOTE, LOG_WARNING
+from shared_utils import nextTick
+from predefined_hosts import g_preDefinedHosts, AUTO_LOGIN_QUERY_URL
+from helpers import getClientLanguage, uniprof
+from account_shared import isValidClientVersion
+from skeletons.connection_mgr import IConnectionManager
+_MIN_RECONNECTION_TIMEOUT = 5
+_RECONNECTION_TIMEOUT_INCREMENT = 5
+_MAX_RECONNECTION_TIMEOUT = 20
+
+class CONNECTION_METHOD(object):
+    BASIC = 'basic'
+    TOKEN = 'token'
+    TOKEN2 = 'token2'
+
+
+class LOGIN_STATUS(object):
+    NOT_SET = 'NOT_SET'
+    LOGGED_ON = 'LOGGED_ON'
+    LOGGED_ON_OFFLINE = 'LOGGED_ON_OFFLINE'
+    CONNECTION_FAILED = 'CONNECTION_FAILED'
+    DNS_LOOKUP_FAILED = 'DNS_LOOKUP_FAILED'
+    UNKNOWN_ERROR = 'UNKNOWN_ERROR'
+    CANCELLED = 'CANCELLED'
+    ALREADY_ONLINE_LOCALLY = 'ALREADY_ONLINE_LOCALLY'
+    LOGIN_REJECTED_BAN = 'LOGIN_REJECTED_BAN'
+    LOGIN_REJECTED_NO_SUCH_USER = 'LOGIN_REJECTED_NO_SUCH_USER'
+    LOGIN_REJECTED_INVALID_PASSWORD = 'LOGIN_REJECTED_INVALID_PASSWORD'
+    SESSION_END = 'SESSION_END'
+    LOGIN_REJECTED_ALREADY_LOGGED_IN = 'LOGIN_REJECTED_ALREADY_LOGGED_IN'
+    LOGIN_REJECTED_BAD_DIGEST = 'LOGIN_REJECTED_BAD_DIGEST'
+    LOGIN_REJECTED_DB_GENERAL_FAILURE = 'LOGIN_REJECTED_DB_GENERAL_FAILURE'
+    LOGIN_REJECTED_DB_NOT_READY = 'LOGIN_REJECTED_DB_NOT_READY'
+    LOGIN_REJECTED_ILLEGAL_CHARACTERS = 'LOGIN_REJECTED_ILLEGAL_CHARACTERS'
+    LOGIN_CUSTOM_DEFINED_ERROR = 'LOGIN_CUSTOM_DEFINED_ERROR'
+    LOGIN_BAD_PROTOCOL_VERSION = 'LOGIN_BAD_PROTOCOL_VERSION'
+    LOGIN_REJECTED_SERVER_NOT_READY = 'LOGIN_REJECTED_SERVER_NOT_READY'
+    LOGIN_REJECTED_RATE_LIMITED = 'LOGIN_REJECTED_RATE_LIMITED'
+
+
+INVALID_TOKEN2_EXPIRED = ('Token2 expires time', 'Invalid token2')
+
+class ConnectionData(object):
+
+    def __init__(self):
+        self.username = None
+        self.password = None
+        self.inactivityTimeout = None
+        self.publicKeyPath = None
+        self.clientContext = None
+        self.peripheryRoutingGroup = None
+        return
+
+
+class ConnectionManager(IConnectionManager):
+
+    def __init__(self):
+        self.__connectionData = ConnectionData()
+        self.__connectionUrl = None
+        self.__connectionMethod = CONNECTION_METHOD.BASIC
+        self.__connectionStatus = LOGIN_STATUS.NOT_SET
+        self.__lastLoginName = None
+        self.__hostItem = g_preDefinedHosts._makeHostItem('', '', '')
+        self.__availableHosts = None
+        self.__lastSessionID = ''
+        self.__retryConnectionPeriod = _MIN_RECONNECTION_TIMEOUT
+        self.__retryConnectionCallbackID = None
+        self.__connectionInProgress = False
+        g_playerEvents.onKickWhileLoginReceived += self.__processKick
+        g_playerEvents.onLoginQueueNumberReceived += self.__processQueue
+        g_playerEvents.onPeripheryRoutingGroupReceived += self.setPeripheryRoutingGroup
+        self.__eManager = EventManager()
+        self.onLoggedOn = Event(self.__eManager)
+        self.onConnected = Event(self.__eManager)
+        self.onRejected = Event(self.__eManager)
+        self.onDisconnected = Event(self.__eManager)
+        self.onKickedFromServer = Event(self.__eManager)
+        self.onKickWhileLoginReceived = Event(self.__eManager)
+        self.onQueued = Event(self.__eManager)
+        self.onPeripheryRoutingGroupUpdated = Event(self.__eManager)
+        return
+
+    def fini(self):
+        g_playerEvents.onPeripheryRoutingGroupReceived -= self.setPeripheryRoutingGroup
+        g_playerEvents.onKickWhileLoginReceived -= self.__processKick
+        g_playerEvents.onLoginQueueNumberReceived -= self.__processQueue
+        self.__eManager.clear()
+        self.stopRetryConnection()
+        return
+
+    def initiateConnection(self, params, password, serverName):
+        self.__setConnectionData(params, password)
+        if serverName == AUTO_LOGIN_QUERY_URL:
+            g_preDefinedHosts.autoLoginQuery(self.__setHostDataAndConnect)
+        else:
+            self.__setHostDataAndConnect(g_preDefinedHosts.byUrl(serverName))
+        return
+
+    def stopRetryConnection(self):
+        if self.__retryConnectionCallbackID is not None:
+            BigWorld.cancelCallback(self.__retryConnectionCallbackID)
+            self.__retryConnectionPeriod = 0
+            self.__retryConnectionCallbackID = None
+        return
+
+    @uniprof.regionDecorator(label='offline.connect', scope='enter')
+    def __connect(self):
+        self.__retryConnectionCallbackID = None
+        if constants.IS_DEVELOPMENT:
+            LOG_DEBUG(('Calling BigWorld.connect with params: {0}, serverName: {1}, inactivityTimeout: {2}, publicKeyPath: {3}').format(self.__connectionData.username, self.__connectionUrl, constants.CLIENT_INACTIVITY_TIMEOUT, self.__connectionData.publicKeyPath))
+        if self.__connectionInProgress:
+            LOG_WARNING('Try to call BigWorld.connect while connection in progress')
+            return
+        else:
+            nextTick(self.__tryConnect)()
+            if g_preDefinedHosts.predefined(self.__connectionUrl) or g_preDefinedHosts.roaming(self.__connectionUrl):
+                self.__hostItem = g_preDefinedHosts.byUrl(self.__connectionUrl)
+            else:
+                for server in BigWorld.serverDiscovery.servers:
+                    if server.serverString == self.__connectionUrl:
+                        self.__hostItem = self.__hostItem._replace(name=server.ownerName, shortName=server.ownerName)
+                        break
+                else:
+                    self.__hostItem = self.__hostItem._replace(name=self.__connectionUrl, shortName=self.__connectionUrl)
+
+            return
+            return
+
+    def __tryConnect(self):
+        if self.__connectionInProgress:
+            LOG_WARNING('Try to call BigWorld.connect while connection in progress, but on nextTick')
+            return
+        self.__connectionInProgress = True
+        LOG_NOTE('[LOCAL SERVER] BigWorld.connect URL: %s' % self.__connectionUrl)
+        LOG_NOTE('[LOCAL SERVER] publicKeyPath: %s' % self.__connectionData.publicKeyPath)
+        LOG_NOTE('[LOCAL SERVER] username: %s' % self.__connectionData.username)
+        LOG_NOTE('[LOCAL SERVER] inactivityTimeout: %s' % self.__connectionData.inactivityTimeout)
+        try:
+            BigWorld.connect(self.__connectionUrl, self.__connectionData, self.__serverResponseHandler)
+            LOG_NOTE('[LOCAL SERVER] BigWorld.connect returned OK')
+        except Exception as e:
+            LOG_NOTE('[LOCAL SERVER] BigWorld.connect FAILED: %s' % str(e))
+
+        return
+
+    @uniprof.regionDecorator(label='offline.connect', scope='exit')
+    def __serverResponseHandler(self, stage, status, responseDataJSON):
+        LOG_NOTE('[LOCAL SERVER] Response: stage=%s status=%s data=%s' % (stage, status, responseDataJSON))
+        status = str(status)
+        self.__connectionInProgress = False
+        self.__connectionStatus = status
+        try:
+            responseData = json.loads(responseDataJSON)
+        except ValueError:
+            responseData = {'errorMessage': responseDataJSON}
+
+        if status == LOGIN_STATUS.LOGGED_ON:
+            if stage == 1:
+                if self.__connectionMethod == CONNECTION_METHOD.TOKEN and 'token2' in responseData:
+                    self.__swtichToToken2(responseData['token2'])
+                self.__lastSessionID = responseData.get('session_id', '')
+                self.onLoggedOn(responseData)
+                self.onConnected()
+        elif self.__retryConnectionCallbackID is None:
+            status_ = self.__connectionStatus
+            errorMsg = responseData.get('errorMessage', '')
+            if errorMsg in INVALID_TOKEN2_EXPIRED:
+                status_ = LOGIN_STATUS.SESSION_END
+            self.onRejected(status_, responseData)
+        if status == LOGIN_STATUS.LOGIN_REJECTED_RATE_LIMITED:
+            self.__reconnect()
+        if stage == 6:
+            self.onDisconnected()
+            g_playerEvents.onDisconnected()
+        return
+
+    def __setConnectionData(self, params, password):
+        self.__connectionMethod = params['auth_method']
+        if 'auth_realm' not in params:
+            params['auth_realm'] = constants.AUTH_REALM
+        m = hashlib.md5()
+        m.update(params['session'])
+        params['session'] = m.hexdigest()
+        if constants.IS_IGR_ENABLED:
+            params['is_igr'] = '1'
+        username_ = json.dumps(params, encoding='utf-8')
+        LOG_NOTE(('User authentication method: {0}').format(params['auth_method']))
+        if 'token2' in params and params['token2']:
+            password = ''
+        else:
+            password = pwd_token.generate(password)
+        if 'allowed_peripheries' in params:
+            g_preDefinedHosts.setAvailablePeripheriesByRoutingGroup([int(x) for x in params['allowed_peripheries'].split() if x.isdigit()])
+        else:
+            LOG_NOTE('Not found allowed_peripheries in params: ', params)
+        self.__connectionData.username = username_
+        self.__connectionData.password = password
+        self.__connectionData.inactivityTimeout = constants.CLIENT_INACTIVITY_TIMEOUT
+        self.__connectionData.clientContext = json.dumps({'lang_id': (getClientLanguage()), 'publication': (params.get('publication'))})
+        if constants.IS_DEVELOPMENT and params['auth_method'] == CONNECTION_METHOD.BASIC and params['login'][0] == '@':
+            try:
+                self.__connectionData.username = params['login'][1:]
+            except IndexError:
+                self.__connectionData.username = params['login']
+
+        return
+
+    def __swtichToToken2(self, token2):
+        self.__connectionMethod = CONNECTION_METHOD.TOKEN2
+        params = json.loads(self.__connectionData.username, encoding='utf-8')
+        params.pop('token', None)
+        params['token2'] = token2
+        params['auth_method'] = CONNECTION_METHOD.TOKEN2
+        self.__connectionData.username = json.dumps(params, encoding='utf-8')
+        return
+
+    def __setHostData(self, predefinedHost):
+        self.__connectionData.publicKeyPath = predefinedHost.keyPath
+        self.__connectionUrl = predefinedHost.urlToken if (self.__connectionMethod == CONNECTION_METHOD.TOKEN or self.__connectionMethod == CONNECTION_METHOD.TOKEN2) and predefinedHost.urlToken else predefinedHost.url
+        return
+
+    def __setHostDataAndConnect(self, predefinedHost):
+        self.__setHostData(predefinedHost)
+        self.__connect()
+        return
+
+    def __reconnect(self):
+        self.stopRetryConnection()
+        self.__retryConnectionCallbackID = BigWorld.callback(self.__getRetryConnectionPeriod(), self.__connect)
+        return
+
+    def __getRetryConnectionPeriod(self):
+        if self.__retryConnectionPeriod != _MAX_RECONNECTION_TIMEOUT:
+            self.__retryConnectionPeriod += _RECONNECTION_TIMEOUT_INCREMENT
+        return self.__retryConnectionPeriod
+
+    def __processKick(self, peripheryID):
+        if peripheryID > 0:
+            host = g_preDefinedHosts.periphery(peripheryID, False)
+            if host is not None:
+                self.__setHostData(host)
+            self.__reconnect()
+        self.onKickWhileLoginReceived(peripheryID)
+        return
+
+    def __processQueue(self, queueNumber):
+        self.onQueued(queueNumber)
+        return
+
+    @property
+    def serverUserName(self):
+        return self.__hostItem.name
+
+    @property
+    def serverUserNameShort(self):
+        return self.__hostItem.shortName
+
+    @property
+    def peripheryID(self):
+        return self.__hostItem.peripheryID
+
+    @property
+    def areaID(self):
+        if not self.isDisconnected():
+            return self.__hostItem.areaID
+        else:
+            return
+
+        return
+
+    @property
+    def url(self):
+        return self.__hostItem.url
+
+    @property
+    def loginName(self):
+        if not self.isDisconnected():
+            return self.__lastLoginName
+        else:
+            return
+
+        return
+
+    @property
+    def lastLoginName(self):
+        return self.__lastLoginName
+
+    @property
+    def lastSessionID(self):
+        return self.__lastSessionID
+
+    @property
+    def databaseID(self):
+        if not self.isDisconnected():
+            return BigWorld.player().databaseID
+        else:
+            return
+
+        return
+
+    @property
+    def connectionMethod(self):
+        return self.__connectionMethod
+
+    @property
+    def peripheryRoutingGroup(self):
+        return self.__connectionData.peripheryRoutingGroup
+
+    @property
+    def availableHosts(self):
+        if self.peripheryRoutingGroup is not None and self.__availableHosts is not None:
+            return [p for p in g_preDefinedHosts.peripheries() if p.peripheryID in self.__availableHosts]
+        else:
+            return g_preDefinedHosts.hosts()
+            return
+
+    def isAvailablePeriphery(self, peripheryID=None):
+        if self.__connectionData.peripheryRoutingGroup is None or self.__availableHosts is None:
+            return True
+        if peripheryID is None:
+            peripheryID = self.peripheryID
+        return peripheryID in self.__availableHosts
+
+    def disconnect(self):
+        BigWorld.disconnect()
+        return
+
+    def setKickedFromServer(self, reason, kickReasonType, expiryTime):
+        self.disconnect()
+        self.onKickedFromServer(reason, kickReasonType, expiryTime)
+        return
+
+    def isDisconnected(self):
+        return self.__connectionStatus != LOGIN_STATUS.LOGGED_ON
+
+    def isStandalone(self):
+        return self.peripheryID == 0
+
+    def isConnected(self):
+        return self.__connectionStatus == LOGIN_STATUS.LOGGED_ON
+
+    def checkClientServerVersions(self, clientVersion, serverVersion):
+        if not isValidClientVersion(clientVersion, serverVersion) or ResMgr.activeContentType() in (constants.CONTENT_TYPE.INCOMPLETE,
+         constants.CONTENT_TYPE.TUTORIAL):
+            LOG_DEBUG('Version mismatch. Client is "%s", server needs "%s".' % (
+             clientVersion, serverVersion))
+            self.onRejected(LOGIN_STATUS.LOGIN_BAD_PROTOCOL_VERSION, {})
+            BigWorld.disconnect()
+        return
+
+    def setLastLogin(self, email):
+        self.__lastLoginName = email
+        return
+
+    def setPeripheryRoutingGroup(self, routingGroup, availableHosts):
+        self.__connectionData.peripheryRoutingGroup = routingGroup
+        self.__availableHosts = availableHosts
+        g_preDefinedHosts.setAvailablePeripheriesByRoutingGroup(availableHosts)
+        self.onPeripheryRoutingGroupUpdated()
+        return
+
+
+return
+
+# okay decompiling C:\Users\fsociety\Desktop\unpack\MY_scripts_unpacked\scripts - client\connection_mgr.pyc
